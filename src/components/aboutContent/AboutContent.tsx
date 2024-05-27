968fe1c4d162e5f33f8df7ee109ab65a46802bf9
kmpartner <kmpartner@outlook.com>
2024-05-28 08:40:39 +0900

    change ui

--- src/components/aboutContent/AboutContent.tsx
+++ src/components/aboutContent/AboutContent.tsx
@@ -11,15 +11,19 @@ export const AboutContent = () => {
       </Typography>
 
       <Typography variant="body2" sx={{ marginBottom: '12px', fontSize: "1rem" }}>
-        By supplying liquidity to the lending pool based on Stellar Blockchain, user will receive interest
+        By supplying liquidity to the lending pools based on Stellar Blockchain, user will receive interest
         (if lended asset is in the reward zone, user can receive BLND token as well). 
       </Typography>
 
       <Typography variant="body2" sx={{ marginBottom: '12px', fontSize: "1rem" }}>
-        Users can withdraw lended assets at any time.
+        Users can withdraw lent assets at any time.
         Users retain control of their assets. The protocol is decentralized, trust-free, and non-custodial. 
       </Typography>
 
+      <Typography variant="body2" sx={{ marginBottom: '12px', fontSize: "1rem" }}>
+        By using smart contracts in Stellar Network, this page allows users to access lending pools on the network.
+        BLND is token for lending pools. It is issued to users of lending pools, and can be used for insuring lending pools.
+      </Typography>
 
       <Typography variant="body2" sx={{ marginBottom: '12px' }}>
         {/* When lenders withdraw their lent assets, they also withdraw any interest they have earned. */}
